89976c04ff87d3ce4b431242d981939444668598
Rianne Streef <56265374+RianneStreef@users.noreply.github.com>
2022-07-25 12:01:40 +0200

    first build

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import "../styles/Footer.css";
 
 const Footer = () => {
-  return <div>Footer info</div>;
+  return <div>Footer info !</div>;
 };
 
 export default Footer;
